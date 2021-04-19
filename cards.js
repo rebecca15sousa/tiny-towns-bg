ce0be49b1217c0a4a493085769ca7312d35dff17
Fred Tomé <freddericofilho@gmail.com>
2021-04-19 17:06:21 -0400

    Updated function for card selection
    Co-Authored-By: Viviane <viviane.sm3@gmail.com>
    Co-Authored-By: Rebecca Sousa <64431799+rebecca15sousa@users.noreply.github.com>

--- cards.js
+++ cards.js
@@ -9,12 +9,16 @@ let buildings = {
   yellowBuildings: [],
   blueBuildings: [],
 }
+//Receives the number of players
+let playerNumber = 2;
 
 //Defines the number of each type of cards
 let commonCardsNumber = 4;
 let purpleCardsNumber = 15;
 let blueCardsNumber = 1;
+let resourceCardsNumber = 15;
 
+//Array Containing one of each different type of card;
 let selectedCards = [];
 
 //Types of common buildings, all of which have 4 copies
@@ -35,7 +39,6 @@ let resourceAll = [];
 //Final resources deck
 let resourceDeck = [];
 
-
 for (let i = 0; i < resourceAmount; i++) {
   for (let j = 0; j < resourcesTypes.length; j++) {
     resourceAll.push(resourcesTypes[j]);
@@ -43,7 +46,7 @@ for (let i = 0; i < resourceAmount; i++) {
 }
 
 //Populating resources decks
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < resourceCardsNumber; i++) {
   resourceDeck[i] = new Image();
   resourceDeck[i].src = 'assets/resource_' + resourceAll[i] + '.png';
 }
@@ -53,7 +56,7 @@ buildings.blueBuildings[0] = new Image();
 buildings.blueBuildings[0].src = 'assets/blue_building (1).png';
 
 //Populating common buildings deck (this one could be optimized including a loop to check an array that contains the different types of common buildings)
-  for (let i = 0; i < generalBuildingTypes.length-1; i++) {
+  for (let i = 0; i < generalBuildingTypes.length; i++) {
     for (let j = 1; j < commonCardsNumber + 1; j++) {
       let buildingType = buildings[generalBuildingTypes[i]+"Buildings"];
       buildingType[j-1] = new Image();
@@ -62,8 +65,7 @@ buildings.blueBuildings[0].src = 'assets/blue_building (1).png';
 }
 
 //Populating special buildings deck
-
-for (let i = 1 ; i < 16 ; i++) {
+for (let i = 1 ; i < purpleCardsNumber + 1 ; i++) {
   buildings.purpleBuildings[i-1] = new Image();
   buildings.purpleBuildings[i-1].src = 'assets/purple_building (' + i + ').png';
 }
@@ -72,4 +74,22 @@ for (let i = 1 ; i < 16 ; i++) {
 for (let i = 0; i < generalBuildingTypes.length; i++) {
   let name = buildings[generalBuildingTypes[i]+"Buildings"];
   selectedCards.push(name[Math.floor(Math.random() * name.length)]);
+}
+
+//Select random card(s) for special building Types
+for (let i = 0; i < specialBuildingTypes.length; i++) {
+  if (specialBuildingTypes[i] == "purple") {
+    for (let j = 0; j < playerNumber; j++) {
+      let randomIndex = Math.floor(Math.random() * buildings.purpleBuildings.length);
+      selectedCards.push(buildings.purpleBuildings[randomIndex]);
+      buildings.purpleBuildings.splice[randomIndex, 1];
+    }
+  }
+
+  if (specialBuildingTypes[i] == "blue") {
+      let randomIndex = Math.floor(Math.random() * buildings.blueBuildings.length);
+      selectedCards.push(buildings.blueBuildings[randomIndex]);
+      buildings.blueBuildings.splice[randomIndex, 1];
+  }
+
 }
