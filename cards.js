909e3ed019ed3004b5b14d5915b0741fcec81f48
Fred Tomé <freddericofilho@gmail.com>
2021-04-19 16:38:12 -0400

    Added initial logic for populating decks of cards and selecting a random one for each
    Co-Authored-By: Viviane <viviane.sm3@gmail.com>
    Co-Authored-By: Rebecca Sousa <64431799+rebecca15sousa@users.noreply.github.com>

--- cards.js
+++ cards.js
@@ -1,38 +1,75 @@
-
-var black_buildings = []
-var green_buildings = []
-var grey_buildings = []
-var orange_buildings = []
-var purple_buildings = []
-var red_buildings = []
-var yellow_buildings = []
-var blue_buildings = []
-
-
-
-//Populating blue buildings deck
-blue_buildings[0] = new Image();
-blue_buildings[0].src = 'assets/blue_building (1).png';
-
-//Populating common buildings deck (this one could be optimized including a loop to check an array that contains the different types of common buildings)
-for (i = 1 ; i < 5 ; i++) {
-  black_buildings[i-1] = new Image();
-  green_buildings[i-1] = new Image();
-  grey_buildings[i-1] = new Image();
-  orange_buildings[i-1] = new Image();
-  red_buildings[i-1] = new Image();
-  yellow_buildings[i-1] = new Image();
-
-  black_buildings[i-1].src = 'assets/black_building (' + i + ').png';
-  green_buildings[i-1].src = 'assets/green_building (' + i + ').png';
-  grey_buildings[i-1].src = 'assets/grey_building (' + i + ').png';
-  orange_buildings[i-1].src = 'assets/orange_building (' + i + ').png';
-  red_buildings[i-1].src = 'assets/red_building (' + i + ').png';
-  yellow_buildings[i-1].src = 'assets/yellow_building (' + i + ').png';
+//Object that holds all different buildings
+let buildings = {
+  blackBuildings: [],
+  greenBuildings: [],
+  greyBuildings: [],
+  orangeBuildings: [],
+  purpleBuildings: [],
+  redBuildings: [],
+  yellowBuildings: [],
+  blueBuildings: [],
 }
 
-//Populating purple buildings deck
-for (i = 1 ; i < 16 ; i++) {
-  purple_buildings[i-1] = new Image();
-  purple_buildings[i-1].src = 'assets/purple_building (' + i + ').png';
+//Defines the number of each type of cards
+let commonCardsNumber = 4;
+let purpleCardsNumber = 15;
+let blueCardsNumber = 1;
+
+let selectedCards = [];
+
+//Types of common buildings, all of which have 4 copies
+let generalBuildingTypes = ['black', 'green', 'grey', 'orange', 'red', 'yellow'];
+
+//Types of special buildings, each of which have a variable number of copies
+let specialBuildingTypes = ['blue', 'purple'];
+
+//Available types of resources
+let resourcesTypes = ['brick', 'glass', 'stone', 'wheat', 'wood'];
+
+//Number of each type of resource
+let resourceAmount = 3;
+
+//Array with all types of resources
+let resourceAll = [];
+
+//Final resources deck
+let resourceDeck = [];
+
+
+for (let i = 0; i < resourceAmount; i++) {
+  for (let j = 0; j < resourcesTypes.length; j++) {
+    resourceAll.push(resourcesTypes[j]);
+  }
+}
+
+//Populating resources decks
+for (let i = 0; i < 15; i++) {
+  resourceDeck[i] = new Image();
+  resourceDeck[i].src = 'assets/resource_' + resourceAll[i] + '.png';
+}
+
+//Populating blue buildings deck
+buildings.blueBuildings[0] = new Image();
+buildings.blueBuildings[0].src = 'assets/blue_building (1).png';
+
+//Populating common buildings deck (this one could be optimized including a loop to check an array that contains the different types of common buildings)
+  for (let i = 0; i < generalBuildingTypes.length-1; i++) {
+    for (let j = 1; j < commonCardsNumber + 1; j++) {
+      let buildingType = buildings[generalBuildingTypes[i]+"Buildings"];
+      buildingType[j-1] = new Image();
+      buildingType[j-1].src = 'assets/' + generalBuildingTypes[i] + '_building (' + j + ').png';
+  }
+}
+
+//Populating special buildings deck
+
+for (let i = 1 ; i < 16 ; i++) {
+  buildings.purpleBuildings[i-1] = new Image();
+  buildings.purpleBuildings[i-1].src = 'assets/purple_building (' + i + ').png';
+}
+
+//Select a random card for each common building type
+for (let i = 0; i < generalBuildingTypes.length; i++) {
+  let name = buildings[generalBuildingTypes[i]+"Buildings"];
+  selectedCards.push(name[Math.floor(Math.random() * name.length)]);
 }
