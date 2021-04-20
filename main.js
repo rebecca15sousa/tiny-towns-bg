84146ffc012a479bcf494ac29c85584d316f14d7
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-04-19 20:29:51 -0400

    Created buttons for making the actions of putting a resource or a building on the clicked square
    Co-Authored-By: Viviane <viviane.sm3@gmail.com>
    Co-Authored-By: Fred Tomé <freddericofilho@gmail.com>

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const boardContainer = document.getElementById("boardContainer");
+const colourBtn = document.getElementById("colourBtn");
+const buildingBtn = document.getElementById("buildingBtn");
 
 function createPlayerBoard() {
     const playerBoard = document.createElement("div");
@@ -19,9 +21,27 @@ function selectSquare() {
     allSquares.forEach(function(square) {
         square.addEventListener("click", function(e) {
             let data = e.target.getAttribute("data-square");
+            colourBtn.style.display = "block";
+            colourBtn.setAttribute("data-square", data);
+            buildingBtn.style.display = "block";
+            buildingBtn.setAttribute("data-square", data);
         });
     });
 }
+
+colourBtn.addEventListener("click", function() {
+    const allSquares = document.querySelectorAll(".board-squares");
+    let index = colourBtn.getAttribute("data-square");
+    console.log(index);
+    colourSquare(allSquares[index]);
+});
+
+buildingBtn.addEventListener("click", function() {
+    const allSquares = document.querySelectorAll(".board-squares");
+    let index = colourBtn.getAttribute("data-square");
+    console.log(index);
+    buildSquare(allSquares[index]);
+});
 
 }
 
