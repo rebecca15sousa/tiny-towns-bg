b760cc70ef5ac4c2948eaf4b574e55585047890c
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-04-20 16:03:13 -0400

    Initial drag and drop commit
    Co-Authored-By: Fred Tomé <freddericofilho@gmail.com>
    Co-Authored-By: Viviane <viviane.sm3@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const boardContainer = document.getElementById("boardContainer");
-const colourBtn = document.getElementById("colourBtn");
-const buildingBtn = document.getElementById("buildingBtn");
+// const colourBtn = document.getElementById("colourBtn");
+// const buildingBtn = document.getElementById("buildingBtn");
+
+const blackBuildingPiece = document.getElementById("blackBuildingPiece");
 
 function createPlayerBoard() {
     const playerBoard = document.createElement("div");
@@ -13,35 +15,57 @@ function createPlayerBoard() {
         playerBoard.appendChild(square);
     }
     boardContainer.appendChild(playerBoard);
-    selectSquare();
-}
-
-function selectSquare() {
     let allSquares = document.querySelectorAll(".board-squares");
     allSquares.forEach(function(square) {
-        square.addEventListener("click", function(e) {
-            let data = e.target.getAttribute("data-square");
-            colourBtn.style.display = "block";
-            colourBtn.setAttribute("data-square", data);
-            buildingBtn.style.display = "block";
-            buildingBtn.setAttribute("data-square", data);
-        });
+        square.addEventListener("dragover", dragOver);
+        square.addEventListener("dragenter", dragEnter);
+        square.addEventListener("dragleave", dragLeave);
+        square.addEventListener("drop", dragDrop);
     });
+    // selectSquare();
 }
 
-colourBtn.addEventListener("click", function() {
-    const allSquares = document.querySelectorAll(".board-squares");
-    let index = colourBtn.getAttribute("data-square");
-    console.log(index);
-    colourSquare(allSquares[index]);
-});
+// function selectSquare() {
+//     let allSquares = document.querySelectorAll(".board-squares");
+//     allSquares.forEach(function(square) {
+//         square.addEventListener("click", function(e) {
+//             let data = e.target.getAttribute("data-square");
+//             colourBtn.style.display = "block";
+//             colourBtn.setAttribute("data-square", data);
+//             buildingBtn.style.display = "block";
+//             buildingBtn.setAttribute("data-square", data);
+//         });
+//     });
+// }
 
-buildingBtn.addEventListener("click", function() {
-    const allSquares = document.querySelectorAll(".board-squares");
-    let index = colourBtn.getAttribute("data-square");
-    console.log(index);
-    buildSquare(allSquares[index]);
-});
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    this.classList.add("black-building-piece");
+}
+        
+
+// colourBtn.addEventListener("click", function() {
+//     const allSquares = document.querySelectorAll(".board-squares");
+//     let index = colourBtn.getAttribute("data-square");
+//     colourSquare(allSquares[index]);
+// });
+
+// buildingBtn.addEventListener("click", function() {
+//     const allSquares = document.querySelectorAll(".board-squares");
+//     let index = colourBtn.getAttribute("data-square");
+//     buildSquare(allSquares[index]);
+// });
 
 function colourSquare(square) {
     square.style.backgroundColor = "black";
