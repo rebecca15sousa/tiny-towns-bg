3988e0433abcf343a96e4b8d6a1557db963b48df
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-04-19 20:22:09 -0400

    Created function that listens for clicks on each board square.
    Co-Authored-By: Viviane <viviane.sm3@gmail.com>
    Co-Authored-By: Fred Tomé <freddericofilho@gmail.com>

--- main.js
+++ main.js
@@ -6,10 +6,23 @@ function createPlayerBoard() {
     for(let i = 0; i < 16; i++) {
         let square = document.createElement("div");
         square.classList.add("board-squares");
+        square.setAttribute("data-square", i);
         square.textContent = (i + 1);
         playerBoard.appendChild(square);
     }
     boardContainer.appendChild(playerBoard);
+    selectSquare();
+}
+
+function selectSquare() {
+    let allSquares = document.querySelectorAll(".board-squares");
+    allSquares.forEach(function(square) {
+        square.addEventListener("click", function(e) {
+            let data = e.target.getAttribute("data-square");
+        });
+    });
+}
+
 }
 
 createPlayerBoard();
